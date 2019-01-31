c403c8090c7ec898394aafafd42334087cb0e776
Javal Nanda <javalnanda@gmail.com>
2019-01-31 17:06:24 +0530

    Split routes out into a separate controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,43 +12,6 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
     
-    router.post("api","acronyms") { req -> Future<Acronym> in
-        return try req.content.decode(Acronym.self)
-            .flatMap(to: Acronym.self) { acronym in
-                return acronym.save(on: req)
-        }
-    }
-    
-    router.get("api","acronyms") { req -> Future<[Acronym]> in
-        return Acronym.query(on: req).all()
-    }
-    
-    router.get("api","acronyms",Acronym.parameter) {
-        req -> Future<Acronym> in
-        return try req.parameters.next(Acronym.self)
-    }
-    
-    router.put("api","acronyms",Acronym.parameter) {
-        req -> Future<Acronym> in
-        
-        return try flatMap(to: Acronym.self,
-        req.parameters.next(Acronym.self),
-        req.content.decode(Acronym.self)) {
-            acronym, updatedAcronym in
-            
-            acronym.short = updatedAcronym.short
-            acronym.long = updatedAcronym.long
-            
-            return acronym.save(on: req)
-        }
-    }
-    
-    router.delete("api","acronyms", Acronym.parameter) {
-        req -> Future<HTTPStatus> in
-        
-        return try req.parameters.next(Acronym.self)
-        .delete(on: req)
-        .transform(to: HTTPStatus.noContent)
-    }
-    
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
 }
